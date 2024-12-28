bc2f10cc12ae5539ed14c51e99088fbff930b4a9
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-12-28 18:03:37 +0100

    Update firebase.service.ts

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -6,16 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDBZMSAfXqoSUQIE7bVeejLbY1Voh0hBMw",
-  authDomain: "by1-1db5a.firebaseapp.com",
-  projectId: "by1-1db5a",
-  storageBucket: "by1-1db5a.appspot.com",
-  messagingSenderId: "1094010415075",
-  appId: "1:1094010415075:web:fbd7f8ef0e9d5bb9fea2d6",
-  measurementId: "G-GYDR2YGDCJ"
-};
-
+// put your firebase config HERE -----------
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
